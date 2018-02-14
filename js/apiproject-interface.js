25bbf00d49bfbd0605817b391078a82d11ab8899
Byron Chang and Natalia Telpukhova <natalia@>
2018-02-14 10:41:28 -0800

    add ajax call to api

---
+++ js/apiproject-interface.js
@@ -0,0 +1,61 @@
+let listItem = function(test) {
+  $('#launches').empty();
+  $('#launches').append(`<ul class="list-group">`);
+  for (let i=0; i<test.length; i++) {
+    $('#launches').append(`
+          <li class="list-group-item"> <strong>${test[i].rocket.rocket_name}</strong> <br>
+        ${test[i].launch_site.site_name_long}<br>Launch Date: ${test[i].launch_date_local}<br>
+        Core Serial: ${test[i].rocket.first_stage.cores[0].core_serial}
+
+        </li>
+    `)
+  }
+  $('#launches').append(`</ul>`);
+}
+
+let blockItem = function(response) {
+  $('#launches').empty();
+  for (let i=0; i<response.length; i++) {
+    $('#launches').append(`
+      <div class="card" style="width: 18rem;">
+        <div class="cardBox">
+          <img class="card-img-top" src="${response[i].links.mission_patch}" alt="Card image cap">
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${response[i].rocket.rocket_name}</h5>
+          <p class="card-text">${response[i].launch_site.site_name_long}</p>
+        </div>
+      </div>
+    `)
+  }
+}
+
+$(document).ready(function() {
+  let url = 'https://api.spacexdata.com/v2/launches';
+  let objectsFromResponse;
+
+  $.ajax({
+    url: url,
+    type: 'GET',
+    data: {
+      format: 'json'
+    },
+    success: function(response) {
+      console.log("before resp: " + objectsFromResponse);
+      objectsFromResponse = response;
+      blockItem(response);
+    },
+    error: function() {
+      $('#errors').text("There was an error processing your request. Please try again.");
+    }
+  });
+
+  $('#listItem').click(function(){
+    listItem(objectsFromResponse);
+  });
+  $('#blockItem').click(function(){
+    blockItem(objectsFromResponse);
+  });
+
+
+});
